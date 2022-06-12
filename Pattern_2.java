7161ce87a0b060e01ed2754d3f8704ff719e7a16
Mohd Mustajab Khan <meet2mustajabkhan@gmail.com>
2022-06-12 23:37:47 +0530

    Update Pattern_2.java

--- Pattern_2.java
+++ Pattern_2.java
@@ -7,7 +7,7 @@ public class Pattern_2 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the Number U want-: ");
         int n = sc.nextInt();
-        for(int i=5;i>=1;i--)
+        for(int i=n;i>=1;i--)
         {
             for(int j=1;j<=i;j++)
             {
