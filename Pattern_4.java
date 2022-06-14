7f2be59884c41f8331c92be6ee9de55ee911639c
Mohd Mustajab Khan <meet2mustajabkhan@gmail.com>
2022-06-14 18:24:19 +0530

    Update Pattern_4.java

--- Pattern_4.java
+++ Pattern_4.java
@@ -7,6 +7,23 @@ public class Pattern_4 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the Number U want-: ");
         int n = sc.nextInt();
+        
+//          for(int i=1;i<=n;i++)
+//         {
+//             for(int j=1;j<=n;j++)
+//             {
+//                 if(j>=i) {
+//                     System.out.print("*");
+//                 }
+//                 else
+//                 {
+//                     System.out.print(" ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+        
+        
         for(int i=1;i<=n;i++)
         {
             for(int k=1;k<=i-1;k++)
